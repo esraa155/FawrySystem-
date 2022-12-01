dce7df13a86a64127b666aca7c1050a2fde44ec7
Esraa Mohamed Abdel star <103047126+esraa155@users.noreply.github.com>
2022-12-01 13:09:50 +0200

    class Customer

---
+++ src/Customer.java
@@ -0,0 +1,4 @@
+
+public class Customer extends User implements Observer {
+	public  void log_in(){}
+}
--- src/Cutomer.java
+++
@@ -1,4 +0,0 @@
-
-public class Cutomer extends User implements Observer {
-	public  void log_in(){}
-}
