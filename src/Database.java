dd42f772a49aaa4ce7bc4b22ac2924527d45fb6a
ibrahimyasser450 <ibrahimyasser450@gmail.com>
2022-12-04 17:43:06 +0200

    update class Database

--- src/Database.java
+++ src/Database.java
@@ -2,4 +2,6 @@ import java.util.ArrayList;
 
 public class Database {
 	public static ArrayList<Customer> customers  = new ArrayList<Customer>();
+	public static ArrayList<Service> services  = new ArrayList<Service>();
+	public static ArrayList<Discount> discount  = new ArrayList<Discount>();
 }
