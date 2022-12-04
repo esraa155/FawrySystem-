0b85a3dc1148b5208cae1629b7af1ba66aba3796
body0550 <bodyhassan0550@gmail.com>
2022-12-04 15:45:04 +0200

    log out costumer

--- src/Customer.java
+++ src/Customer.java
@@ -39,6 +39,48 @@ public class Customer extends Account implements Observer {
 	}
 
 	public String Log_out() {
+	for (Customer customer : Database.customers) {
+            if (customer.getUsername().equals(this.username)) {
+                customer.setLoggedIn(false);
+                return "Logged out";
+            }
+        }
 		return null;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
 	}
 }
