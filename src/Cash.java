5e09387b6ca95ce1155e0037a231eb76c8818637
ibrahimyasser450 <ibrahimyasser450@gmail.com>
2022-12-04 17:21:17 +0200

    update class cash

--- src/Cash.java
+++ src/Cash.java
@@ -1,3 +1,22 @@
 public class Cash extends Payment{
+	 private double amount;
 
+		public Cash(double amount) {
+			this.amount = amount;
+		}
+
+	    public void setAmount(double amount) {
+	        this.amount = amount;
+	    }
+
+	    public double getAmount() {
+	        return amount;
+	    }
+
+	    public void pay(double paymentAmount) {
+	        System.out.println("Paying " + paymentAmount + " using Cash.");
+	        setAmount(getAmount() - paymentAmount);
+	        System.out.print("amount :" + getAmount() + "\n");
+
+	    }
 }
