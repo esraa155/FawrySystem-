40fb6fec2af43c6c7132a63a6f239aedf6dc6d93
ibrahimyasser450 <ibrahimyasser450@gmail.com>
2022-12-04 17:49:22 +0200

    update class Payment

--- src/Payment.java
+++ src/Payment.java
@@ -1,4 +1,13 @@
-
 public abstract class Payment {
+	public double amount;
 
+    public abstract void pay(double paymentAmount);
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
