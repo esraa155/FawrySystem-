612eb40ef8ca8fd2f6eca5301dff6bc1b78ac621
ibrahimyasser450 <ibrahimyasser450@gmail.com>
2022-12-04 18:02:28 +0200

    update class wallet

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,4 +1,20 @@
-
 public class Wallet extends Payment{
+	private double amount;
+	
+	Wallet(){
+		this.amount=100000;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 
+		public double getAmount() {
+			return amount;
+		}
+
+		public void pay(double paymentAmount) {
+			 System.out.println("Paying " + paymentAmount + " using Wallet.");
+	         setAmount(getAmount() - paymentAmount);
+				System.out.print("amount :" +getAmount()+"\n");
+		}
 }
