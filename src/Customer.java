c07c0bb1b833635181b23d952cc8b9adca832204
body0550 <bodyhassan0550@gmail.com>
2022-12-04 15:38:07 +0200

    log in costumer

--- src/Customer.java
+++ src/Customer.java
@@ -1,13 +1,44 @@
+import java.util.ArrayList;
 
 public class Customer extends Account implements Observer {
 
+	private String email;
+	   private String username;
+	   private String password;
+	   private String phonenumber;
+	   private double amount;
+	   private boolean loggedIn = false;
+	   
+	   public Customer() {
+	     email = "";
+	     username = "";
+	     password = "";
+	     phonenumber = "";
+	     amount = 1000.0;
+	   }
+	   public Customer(String email, String username, String password, String phonenumber ) {
+	     this.email = email;
+	     this.username = username;
+	     this.password = password;
+	     this.phonenumber = phonenumber;
+	     this.amount = 1000.0;
+	   }
 	public Account log_in(String username, String password) {
-		// TODO Auto-generated method stub
+		 for(Customer customer : Database.customers) {
+		       if (customer.username.equals(username)) { 
+		        if(customer.password.equals(password)) {
+		          return customer;
+		        }else {
+		          System.out.println( "Check your password");
+		          return null;
+		        }
+		      }
+		     }
+		     System.out.println( "Your username is not found");
 		return null;
 	}
 
 	public String Log_out() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,5 @@
+import java.util.ArrayList;
 
 public class Database {
-
+	public static ArrayList<Customer> customers  = new ArrayList<Customer>();
 }
