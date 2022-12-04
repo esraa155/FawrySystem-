73d266a9ee54ab97897d9fb885db4fa56ada4901
ibrahimyasser450 <ibrahimyasser450@gmail.com>
2022-12-04 22:44:50 +0200

    update class Donation

--- src/Donation.java
+++ src/Donation.java
@@ -1,4 +1,18 @@
-
 public class Donation  implements Service{
-
+	DonationProvider Dp;
+	Donation(){
+	    
+	}
+	    public void createProvider(int option){
+	        if(option == 1){
+	            Dp = new NGO();
+	        }
+	        else if(option == 2){
+	            Dp = new Hospital();
+	        }
+	        else if(option == 3){
+	            Dp = new School();
+	        }
+	      
+	    }
 }
