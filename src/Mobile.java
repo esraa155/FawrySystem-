f2d82849924eab9042574f700492fe9a568d2d54
Kariman Ayman Abd El Latif <karimanaymankaram@gmail.com>
2022-12-05 02:18:50 +0200

    Update Mobile & Observer

--- src/Mobile.java
+++ src/Mobile.java
@@ -1,4 +1,22 @@
-
 public class Mobile  implements Service{
 
+    ServiceProvider prov;
+    Mobile(){
+
+    }
+    public void createProvider(int option){
+        if(option == 1){
+            prov = new WEMobile();					
+        }
+        else if(option == 2){
+            prov = new VodafoneMobile();
+        }
+        else if(option == 3){
+            prov = new EtisalatMobile();
+        }
+        else if(option == 4){
+            prov = new OrangeMobile();
+        }
+    }
 }
+
