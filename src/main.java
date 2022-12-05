f5d089bd1484a56cb6872c36833a62f9e487f617
Kariman Ayman Abd El Latif <karimanaymankaram@gmail.com>
2022-12-05 02:49:07 +0200

    Update Main

--- src/main.java
+++ src/main.java
@@ -1,9 +1,26 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+    Service mobile = new Mobile();
+    Service internet = new Internet();
+	Service landline = new Landline();
+	Service donation = new Donation();
+	Database.services.add(mobile);
+	Database.services.add(internet);
+	Database.services.add(landline);
+	Form f=new Form();
+	Database.services.add(donation);
+    ArrayList<String> servicesName  = new ArrayList<String>();
+	servicesName.add("Mobile");
+	servicesName.add("Internet");
+	servicesName.add("Landline");
+	servicesName.add("Dontion");
 
 	Account a;
 	Customer c;
@@ -12,6 +29,7 @@ public class main {
 	Admin admin = Admin.getInstance();
 	Scanner input = new Scanner(System.in);
 	int choice;
+	int ch;
 	String s;
 	
 	while (true) {
@@ -30,23 +48,80 @@ public class main {
 					admin = null;
 					System.out.println("Logged In");
 					System.out.println("========================================");
-					System.out.println("Are you want to Logout y/n");
-					s = input.nextLine();
-					if (loggedInCustomer != null) {
-				        if (s.equals("yes")) {
-							System.out.println(loggedInCustomer.Log_out());
-						}
-						else
-						{
-							System.out.println("anaaaaaaa");
-						}
-					}
-				} else {
-					System.out.println("========================================");
-					continue;
-				}
+					while(true){
+						System.out.println("1/ services \n2/ serach\n3/ logout");
+						 ch = Integer.parseInt(input.nextLine());
+						  if (ch == 1) 
+						  {
+							   while(true)
+								{
+								   System.out.println("1/ Mobile \n2/ Internet \n3/ Landline \n4/ Donation \n5/ exit for services");
+								    int Option;
+									Option = Integer.parseInt(input.nextLine());
+									f.createService(Option);
+									if(Option==1)
+									{
+										System.out.println("1/ WE   \n2/ Vodafone \n3/ Etisalat \n4/ Orange");
+										int opt;
+										opt = Integer.parseInt(input.nextLine());
+										Mobile mob=new Mobile();
+										mob.createProvider(opt);
+									 }
+									 else if(Option==2)
+									 {
+										System.out.println("1/ WE   \n2/ Vodafone \n3/ Etisalat \n4/ Orange");
+										int opt;
+										opt = Integer.parseInt(input.nextLine());
+										Internet Inter=new Internet();
+										Inter.createProvider(opt);
+									 }
+									 else if(Option==3)
+									 {
+										System.out.println("1/Quarterly   \n2/ Monthly ");
+										int opt;
+										opt = Integer.parseInt(input.nextLine());
+										Landline land=new Landline();
+										land.createProvider(opt);
+									 }
+									 else if(Option==4){
+										System.out.println("1/NGO  \n2/Hospital \n3/School ");
+										int opt;
+										opt = Integer.parseInt(input.nextLine());
+										Donation Doan=new Donation();
+										Doan.createProvider(opt);
+									 }
+									 else if(Option==5)
+									 {
+										System.out.println("ended services");
+										break;
+								     }
+								  }
+							  }
+							  else if(ch==2) 
+							  {
+								  System.out.println("Enter Service name you want ");
+								  String name_ = input.nextLine();
+								for(String names: servicesName){
+                                    if(names.contains(name_)){
+									System.out.println("Service "+name_+" is Found");
+									break;
+									}
+								}
+								System.out.println("Service "+name_+" is not Found");
+								
+							  }
+							  if (ch==3) 
+							  {
+								 System.out.println(loggedInCustomer.Log_out());
+								  break;
+							  }
+							  System.out.println("========================================");
+							  continue;
+				    } 
+				} 
 				break;
-			} else if (choice == 2) {
+			}
+			else if (choice == 2) {
 				System.out.println("Username : ");
 				String username = input.nextLine();
 				System.out.println("Password : ");
